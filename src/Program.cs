71204dfbfa9b1146ed393892718528a03e3c9e49
David Piqueres <dapapi0@gmail.com>
2017-02-22 22:15:55 +0000

    Skeleton for testing environment

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var header = Console.ReadLine().Split().ToList().Select(int.Parse).ToList();
+            int nrows = header[0];
+
+            for(int i=0; i<nrows;i++)
+            {
+                var line = Console.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
